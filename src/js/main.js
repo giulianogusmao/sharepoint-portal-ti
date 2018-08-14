1b700a9cadb03c46c1554a14f88cfd36169b6675
Giuliano Gusmão <gusmaoggg@gmail.com>
2018-08-14 16:58:30 -0300

    Alterando animação menu-mobile; finalizando carousel; espac. indicadores;

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,17 @@
 $(function () {
-    $('button.navbar-toggler').on('click', () => {
-        $('.pti-header-group').toggleClass('pti-menu-show');
+    // adiciona/remove class ao header quando abre menu mobile
+    const toogleMenu = () => $('.pti-header-group').toggleClass('pti-menu-show');
+
+    // abre/fecha menu mobile
+    $('button.navbar-toggler').on('click', toogleMenu);
+
+    // fecha menu mobile quando clicar fora
+    $(document).on('click', '.pti-menu-show', (event) => {
+        if ($(event.target).hasClass('pti-menu-show')) {
+            $('button.navbar-toggler').trigger('click');
+        }
     });
 
+    // ativa tooltips
     $('[data-toggle="tooltip"]').tooltip();
 });
