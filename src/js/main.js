588145ef77148b89b1c6f76671f51ad60a8e0bcb
Giuliano Gusmao <giulianoggg@hotmail.com>
2018-10-02 17:58:35 -0300

    forcando sharepoint a respeitar o tamanho do conteudo sem scroll

--- src/js/main.js
+++ src/js/main.js
@@ -17,63 +17,8 @@ $(function () {
     // ativa tooltips
     $('[data-toggle="tooltip"]').tooltip();
 
-
-    // Exibir/Ocultar links úteis
-    (function () {
-        var $footer = $('.pti-main.pti-footer'),
-            $divLinks = $footer.find('.pti-links-uteis'),
-            $listLinks = $footer.find('.pti-footer-list'),
-            showClass = 'pti-show-links-uteis';
-
-        var toggleFooter = function (expandir) {
-            if (expandir === true || (expandir === undefined && !$footer.hasClass(showClass))) {
-                $footer.addClass(showClass);
-                $divLinks.css({ 'height': $listLinks.outerHeight() });
-            } else {
-                $footer.removeClass(showClass);
-                $divLinks.css({ 'height': 0 });
-            }
-        }
-
-        // abre e fecha footer quando clicar no btn-links-uteis
-        $('.pti-btn-links-uteis').on('click', function (event) {
-            toggleFooter();
-        });
-
-        let timerscroll = 0;
-        // expandir footer quando scroll chegar no final
-        $(window).scroll(function () {
-            // reduz a quantidade que vezes que a validação de scroll é invocada
-            clearTimeout(timerscroll);
-            timerscroll = setTimeout(function () {
-                var hgFooter = $footer.outerHeight(),
-                    stFooter = $(document).height() - hgFooter,
-                    posFooter = stFooter + hgFooter - 2,
-                    st = $(window).scrollTop(),
-                    wHeight = $(window).height(),
-                    posScroll = wHeight + st;
-
-                // console.log(`${posScroll} > ${posFooter}`);
-                if (posScroll > posFooter) {
-                    // Scroll Down - show footer
-                    if (!$footer.hasClass(showClass)) {
-                        toggleFooter(true);
-                        // setTimeout(function () {
-                        //     $('html, body').stop().animate({ scrollTop: $(document).height() }, 500);
-                        // }, 150);
-                    }
-                } else {
-                    // hide footer
-                    if ($footer.hasClass(showClass)) {
-                        toggleFooter(false);
-                    }
-                }
-            }, 200);
-        });
-    })();
-
     // esconde overlay carregando...
-    (function() {
+    (function () {
         setTimeout(() => {
             $('.pti-overlay-loading').addClass('pti-ocultar-loading');
         }, 1000);
